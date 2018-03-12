432c966d8020808a2f5df68751b775d794389adb
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-13 00:01:56 +0300

    getting longitude and latitude

--- locate/views.py
+++ locate/views.py
@@ -11,7 +11,11 @@ print(place_result)
 sample url >> https://maps.googleapis.com/maps/api/geocode/json?address=uhuru+park&key=AIzaSyA7DsJI3ri1eXlyu8wyBfooWw4FZVNmafc
 """
 geo_result = gmaps.geocode('uhuru park')
-print(geo_result)
+geometry = geo_result[0]['geometry']
+location = geometry['location']
+lat = location.get('lat')
+lng = location.get('lng')
+print(lat, lng)
 # Create your views here.
 def home(request):
     test = "Working!"
