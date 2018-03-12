fbab30ed220a58c1c9087aafc11421d9e96acac3
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-12 14:49:54 +0300

    adding static file connection code

--- locate/urls.py
+++ locate/urls.py
@@ -3,13 +3,17 @@ We define our app routes here. We import;
 
 1. url - This will allow us to define our url paths using django's inbuilt url method.
 2. views - This allows us to connect a path to the view function rendering it.
-
+3. settings - this allows us to connect the static files to our app.
+4. static
 """
 
 from django.conf.urls import url
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$', views.home, name='home'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
