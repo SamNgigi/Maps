eaca734fa70cf0c0ab56490566d216c13e4272da
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-12 23:48:21 +0300

    results being displayed in terminal

--- locate/views.py
+++ locate/views.py
@@ -3,7 +3,10 @@ import googlemaps
 from datetime import datetime
 
 gmaps = googlemaps.Client(key='AIzaSyA7DsJI3ri1eXlyu8wyBfooWw4FZVNmafc')
-
+# place_result = gmaps.places('nairobi')
+# print(place_result)
+geo_result = gmaps.geocode('nairobi')
+print(geo_result)
 # Create your views here.
 def home(request):
     test = "Working!"
