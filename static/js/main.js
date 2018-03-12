0107d09f29b53f4da504ac02a210a70bca5952e7
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-12 21:52:37 +0300

    adding markers succesful

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,7 @@ var map, infoWindow;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 17
+        zoom: 14
     });
 
       // Add Marker Function
