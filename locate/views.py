c56110b31ca136e2e712dd2d77e416c0e91c61e5
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-13 11:10:47 +0300

    added comments

--- locate/views.py
+++ locate/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 import googlemaps
 from datetime import datetime
 
+# We use python's googlemap client to make the api request. We pass in the api key.
+
 gmaps = googlemaps.Client(key='AIzaSyA7DsJI3ri1eXlyu8wyBfooWw4FZVNmafc')
 
 """
@@ -10,14 +12,21 @@ print(place_result)
 
 sample url >> https://maps.googleapis.com/maps/api/geocode/json?address=uhuru+park&key=AIzaSyA7DsJI3ri1eXlyu8wyBfooWw4FZVNmafc
 """
-geo_result = gmaps.geocode('nyayo stadium')
+# We then choose the api we want to interact with. In this case it is googlemap's geocoding api. The code below does a similar request and the url above
+geo_result = gmaps.geocode('uhuru park')
+# We go through the response we get from the api and get the location coordinates.
+# What we have below is similar to;
+# latitude = geo_result[0]['geometry']['location'].get('lat),
+# longitude = geo_result[0]['geometry']['location'].get('lng), 
 geometry = geo_result[0]['geometry']
 location = geometry['location']
 lat = location.get('lat')
 lng = location.get('lng')
+# We print see the response we get.
 print(lat, lng)
 # Create your views here.
 def home(request):
+    # We pass in the coordinates we got from our api-call.
     test = "Working!"
     content = {
         "test": test,
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,21 @@
-console.log('working');
 // GOOGLE MAPS
+
+// This code is not being used. It is here for referal purpose and just in case.
+
+console.log('working');
+
+
+
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
+
+/* 
+We initialize the map and the infoWindow vars.
+
+Below we are also storing our views.py variables as javascript vars that we can then pass in as map variables.
+*/ 
 var map, infoWindow;
 var lat = "{{latitude}}";
 latitude = parseFloat(lat)
@@ -13,6 +25,14 @@ var lng = "{{longitude}}";
 longitude = parseFloat(lng);
 console.log(longitude)
 
+/* 
+The initMap function is responsible for rendering the map. It has a couple of functions within it.
+
+1. We store the map configurations in our map var. i.e the longitude and latitude and we also set the map zoom.
+2. We create the addMarker function for creating a marker
+3. We also add an event listener so that a marker appears where we click.
+*/ 
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
@@ -21,6 +41,8 @@ function initMap() {
         },
         zoom: 14
     });
+
+
 
     // Add Marker Function
     function addMarker(props) {
